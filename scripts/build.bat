7e86513a0eecc0ae4b81f2b94ae7e9a1cc01ef78
SamLolo <sam.e.townley@icloud.com>
2025-07-12 15:10:07 +0100

    Improved build batch file

--- scripts/build.bat
+++ scripts/build.bat
@@ -1,8 +1,42 @@
+:: Batch file used to build the current pack files.
+:: Updated: 12/07/2025
+
+@echo OFF
+CLS
+TITLE Modpack Build
+
+:: Create out directory if it doesn't already exist
+echo "Starting build:"
 cd ..
+if exist out\ (
+    echo "Found existing output. Removing old files..."
+    rmdir /S /Q out
+) || echo "Creating new output folder"
 mkdir out
-cd curseforge/fabric
-packwiz cf export
-move Feather-*.zip ../../out
-cd ../../modrinth/fabric
-packwiz mr export
-move Feather-*.mrpack ../../out
+
+:: Build Curseforge files
+echo: & echo "Building curseforge files"
+cd curseforge
+FOR %%A IN (fabric) DO (
+    echo "Start build: %%A ->"
+    cd %%A
+    packwiz cf export
+    move Feather-*.zip ../../out
+    echo "%%A build complete!" & echo:
+    cd ..
+)
+
+:: Build Modrinth files
+echo "Building curseforge files"
+cd ../modrinth
+FOR %%A IN (fabric) DO (
+    echo "Start build: %%A ->"
+    cd %%A
+    packwiz mr export
+    move Feather-*.mrpack ../../out
+    echo "%%A build complete!" & echo:
+    cd ..
+)
+
+echo "Script completed!"
+PAUSE
